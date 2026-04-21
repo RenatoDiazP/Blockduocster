c514cedeba03db8bbecdb3cdd63f6258d108e4f4
RenatoDiazP <re.diazp@duocuc.cl>
2026-04-21 09:28:26 -0400

    Commit de ms
    Primer commit de los microservicios

---
+++ renta/src/main/java/cl/blockduocster/renta/RentaApplication.java
@@ -0,0 +1,13 @@
+package cl.blockduocster.renta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RentaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RentaApplication.class, args);
+	}
+
+}
---
+++ renta/src/test/java/cl/blockduocster/renta/RentaApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.blockduocster.renta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RentaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
