def9385051009d024c778d7de3673ea6868a7eb1
RenatoDiazP <re.diazp@duocuc.cl>
2026-04-21 10:17:48 -0400

    Create Stock.java
    modelo de stock, me faltan anotaciones

---
+++ inventario/src/main/java/cl/blockduocster/inventario/model/Stock.java
@@ -0,0 +1,18 @@
+package cl.blockduocster.inventario.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "stock")
+public class Stock {
+
+    private int idPelicula;
+    private int stockLeft;
+}
