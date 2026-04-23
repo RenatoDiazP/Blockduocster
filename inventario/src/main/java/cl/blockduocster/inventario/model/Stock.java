1c4069fd20e173d94cec6fb188cc21a68d494ff3
RenatoDiazP <re.diazp@duocuc.cl>
2026-04-23 08:49:28 -0400

    Update Stock.java
    le di un Id a inventario, para no generar ids nuevos para las peliculas (y que haya mismatch). Le di nullable = false a idPelicula

--- inventario/src/main/java/cl/blockduocster/inventario/model/Stock.java
+++ inventario/src/main/java/cl/blockduocster/inventario/model/Stock.java
@@ -1,6 +1,10 @@
 package cl.blockduocster.inventario.model;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -12,7 +16,12 @@ import lombok.NoArgsConstructor;
 @Entity
 @Table(name = "stock")
 public class Stock {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int idRegistro;
 
+    @Column(nullable = false)
     private int idPelicula;
+
     private int stockLeft;
 }
