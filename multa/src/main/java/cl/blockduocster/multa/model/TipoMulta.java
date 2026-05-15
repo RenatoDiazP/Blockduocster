cde3fe6b2dac248f429bf0843d680db5904d9acd
Renato Díaz <re.diazp@duocuc.cl>
2026-05-15 18:51:18 -0400

    Tipos Multa
    Se me olvido subir el cambio

--- multa/src/main/java/cl/blockduocster/multa/model/TipoMulta.java
+++ multa/src/main/java/cl/blockduocster/multa/model/TipoMulta.java
@@ -1,11 +1,9 @@
 package cl.blockduocster.multa.model;
 
-import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -15,17 +13,11 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
-@Table(name = "multa")
-public class Multa {
+@Table(name = "tipo_multa")
+public class TipoMulta {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int idMulta;
+    private int idTipo;
 
-    @Column(nullable = false)
-    private int idCliente;
-
-    @ManyToOne
-    private TipoMulta tipo;
-
-    private int montoMulta;
+    private String tipoMulta;
 }
