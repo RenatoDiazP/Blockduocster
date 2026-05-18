0c94c2953ecbd64ff581ff989ce0e1de73940c4c
Renato Díaz <re.diazp@duocuc.cl>
2026-05-18 18:45:58 -0400

    PeliculaDTO
    Cree DTO en stock

---
+++ inventario/src/main/java/cl/blockduocster/inventario/DTO/PeliculaDTO.java
@@ -0,0 +1,10 @@
+package cl.blockduocster.inventario.DTO;
+
+import lombok.Data;
+
+@Data
+public class PeliculaDTO {
+    private int idPelicula;
+    private String nombrePelicula;
+    private String genero;
+}
