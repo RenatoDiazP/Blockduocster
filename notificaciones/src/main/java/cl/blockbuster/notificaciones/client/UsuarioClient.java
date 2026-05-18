427c525f50841a5b55efab104f26d97d4191f359
JuanAliaga81 <ju.aliagac@duocuc.cl>
2026-05-18 19:49:15 -0400

    Update UsuarioClient.java

--- notificaciones/src/main/java/cl/blockbuster/notificaciones/client/UsuarioClient.java
+++ notificaciones/src/main/java/cl/blockbuster/notificaciones/client/UsuarioClient.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 import cl.blockbuster.notificaciones.dto.UsuarioDTO;
 
-@FeignClient(name = "usuarios", url = "http://localhost:8081")
+@FeignClient(name = "usuarios", url = "http://localhost:8080")
 public interface UsuarioClient {
     @GetMapping("/appi/v1/usuarios/dto/{id}")
     UsuarioDTO buscarPorId(@PathVariable("id") Integer id);
