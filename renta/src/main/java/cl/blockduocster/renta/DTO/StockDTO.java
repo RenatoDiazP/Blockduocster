2157a3ce0ce5d3f7ed536a41614e58670b9808d8
Renato Díaz <re.diazp@duocuc.cl>
2026-05-18 18:24:34 -0400

    StockDTO
    Me falto un DTO, ahora si

---
+++ renta/src/main/java/cl/blockduocster/renta/DTO/StockDTO.java
@@ -0,0 +1,10 @@
+package cl.blockduocster.renta.DTO;
+
+import lombok.Data;
+
+@Data
+public class StockDTO {
+    private int idRegistro;
+    private int idPelicula;
+    private int stockLeft;
+}
