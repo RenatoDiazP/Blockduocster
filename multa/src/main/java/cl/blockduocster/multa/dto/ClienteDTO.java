162f5662a4a3c3e447938019cb3d64f7282cdbd7
Renato Díaz <re.diazp@duocuc.cl>
2026-05-18 19:03:52 -0400

    ClienteDTO en Multa
    Necesario

---
+++ multa/src/main/java/cl/blockduocster/multa/dto/ClienteDTO.java
@@ -0,0 +1,10 @@
+package cl.blockduocster.multa.dto;
+
+import lombok.Data;
+
+@Data
+public class ClienteDTO {
+    private int idCliente;
+    private String nombreCliente;
+    private String emailCliente;
+}
