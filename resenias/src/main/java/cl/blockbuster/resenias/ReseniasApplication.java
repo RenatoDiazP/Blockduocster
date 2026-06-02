60f891ba155f9de79eef8d3b57913ed450a2069d
JuanAliaga81 <ju.aliagac@duocuc.cl>
2026-06-02 11:49:31 -0400

    Update ReseniasApplication.java

--- resenias/src/main/java/cl/blockbuster/resenias/ReseniasApplication.java
+++ resenias/src/main/java/cl/blockbuster/resenias/ReseniasApplication.java
@@ -4,11 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableFeignClients
 public class ReseniasApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ReseniasApplication.class, args);
-		System.out.println("WORKINGGGGGGGGGGGGGGGGGGG");
+		System.out.println("Funcionando - resenias");
 	}
 
 }
